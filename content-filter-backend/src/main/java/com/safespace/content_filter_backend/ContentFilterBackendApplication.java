b44456ad9e763dc17e3ca98ed94f0b6e5e28b53e
rush020114 <rush020114@gmail.com>
2025-11-01 08:37:28 +0900

    프로젝트 백엔드  프론트엔드 기본 세팅 완료

---
+++ content-filter-backend/src/main/java/com/safespace/content_filter_backend/ContentFilterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.safespace.content_filter_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentFilterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentFilterBackendApplication.class, args);
+	}
+
+}
---
+++ content-filter-backend/src/test/java/com/safespace/content_filter_backend/ContentFilterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safespace.content_filter_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentFilterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
