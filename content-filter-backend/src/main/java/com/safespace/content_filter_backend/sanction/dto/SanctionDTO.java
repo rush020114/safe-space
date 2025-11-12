c265614dc5b5edf4e31cfe78b88040bc32a484b4
rush020114 <rush020114@gmail.com>
2025-11-13 06:22:06 +0900

    제재 테이블 dto, resultmap세팅

--- content-filter-backend/src/main/java/com/safespace/content_filter_backend/sanction/dto/SanctionDTO.java
+++ content-filter-backend/src/main/java/com/safespace/content_filter_backend/sanction/dto/SanctionDTO.java
@@ -1,4 +1,23 @@
 package com.safespace.content_filter_backend.sanction.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
 public class SanctionDTO {
+  private int sanctionId;
+  private String sanctionType;
+  private String sanctionReason;
+  private LocalDateTime startDate;
+  private LocalDateTime endDate;
+  private int memId;
+  private int adminId;
+
+  public SanctionDTO(){
+  }
 }
